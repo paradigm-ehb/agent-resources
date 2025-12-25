4757d0ac78fe04faa3eac000fa74e4c7195bd90a
nasr <nsrddyn@gmail.com>
2025-12-25 09:58:19 +0100

    feature: removed opaque types and small refactor

--- agent_resources.h
+++ agent_resources.h
@@ -1,4 +1,3 @@
-
 #ifndef RESOURCES_H
 #define RESOURCES_H
 
@@ -33,13 +32,8 @@ typedef int64_t i64;
 typedef i16 b16;
 typedef i32 b32;
 
-/**
- * Mem sizes for Arena Allocation
- * */
 static inline u64 KiB(u64 n) { return n << 10; }
-
 static inline u64 MiB(u64 n) { return n << 20; }
-
 static inline u64 GiB(u64 n) { return n << 30; }
 
 typedef struct mem_arena mem_arena;
@@ -48,9 +42,72 @@ typedef struct Cpu Cpu;
 typedef struct Ram Ram;
 typedef struct Disk Disk;
 typedef struct Device Device;
-typedef struct Proces Proces;
 
-mem_arena *arena_create(uint64_t capacity);
+typedef struct Partition Partition;
+typedef struct Process Process;
+typedef struct Process_List Process_List;
+
+typedef enum Process_State {
+  PROCESS_UNDEFINED = 'N',
+  PROCESS_RUNNING = 'R',
+  PROCESS_SLEEPING = 'S',
+  PROCESS_DISK_SLEEP = 'D',
+  PROCESS_STOPPED = 'T',
+  PROCESS_TRACING_STOPPED = 't',
+  PROCESS_ZOMBIE = 'Z',
+  PROCESS_DEAD = 'X',
+} Process_State;
+
+struct Process {
+  i32 pid;
+  Process_State state;
+
+  u64 utime;
+  u64 stime;
+
+  u32 num_threads;
+
+  char name[BUFFER_SIZE_SMALL];
+};
+
+struct Process_List {
+  Process *items;
+  size_t count;
+  size_t capacity;
+};
+
+struct Partition {
+  u64 major;
+  u64 minor;
+  u64 blocks;
+  char name[BUFFER_SIZE_SMALL];
+};
+
+struct Cpu {
+  char vendor[BUFFER_SIZE_DEFAULT];
+  char model[BUFFER_SIZE_DEFAULT];
+  char frequency[BUFFER_SIZE_SMALL];
+  char cores[BUFFER_SIZE_SMALL];
+};
+
+struct Ram {
+  char total[BUFFER_SIZE_DEFAULT];
+  char free[BUFFER_SIZE_DEFAULT];
+};
+
+struct Disk {
+  Partition *partitions;
+  size_t count;
+  size_t capacity;
+};
+
+struct Device {
+  char os_version[BUFFER_SIZE_DEFAULT];
+  char uptime[BUFFER_SIZE_DEFAULT];
+  Process_List processes;
+};
+
+mem_arena *arena_create(u64 capacity);
 void arena_destroy(mem_arena *arena);
 void arena_clear(mem_arena *arena);
 
@@ -61,27 +118,13 @@ Device *device_create(mem_arena *arena);
 
 int cpu_read(Cpu *cpu);
 int ram_read(Ram *ram);
-int disk_read(Disk *disk, mem_arena *m);
+int disk_read(Disk *disk, mem_arena *arena);
 int device_read(Device *device);
 
-char *cpu_get_vendor(Cpu *cpu);
-char *cpu_get_model(Cpu *cpu);
-char *cpu_get_frequency(Cpu *cpu);
-char *cpu_get_cores(Cpu *cpu);
+int process_list_collect(Process_List *list, mem_arena *arena);
 
-char *ram_get_total(Ram *ram);
-char *ram_get_free(Ram *ram);
+int process_read(i32 pid, Process *out);
 
-size_t disk_get_count(Disk *disk);
-char *disk_get_partitions(Disk *disk);
+int process_kill(i32 pid, i32 signal);
 
-char *device_get_os_version(Device *device);
-char *device_get_uptime(Device *device);
-char **device_get_procs(Device *device);
-size_t device_get_procs_count(Device *device);
-
-void collect_processes(Device *dev, mem_arena *m);
-
-int process_kill(int pid, int signal);
-
-#endif
+#endif /* RESOURCES_H */
